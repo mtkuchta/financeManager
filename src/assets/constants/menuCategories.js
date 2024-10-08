f3ec7b9ae2090c0b147d16c410b1f2cfe0e69663
Mateusz Kuchta <mtkuchta@gmail.com>
2024-10-08 20:50:11 +0200

    menuCategories refactor

--- src/assets/constants/menuCategories.js
+++ src/assets/constants/menuCategories.js
@@ -3,9 +3,9 @@ import iconTransactions from "../icons/icon-nav-transactions.svg";
 import iconBudgets from "../icons/icon-nav-budgets.svg";
 import iconPots from "../icons/icon-nav-Pots.svg";
 
-export const menuCategories = {
-  OVERVIEW: { route: "/overview", icon: iconOverview },
-  TRANSACTIONS: { route: "/transactions", icon: iconTransactions },
-  BUDGETS: { route: "/budgets", icon: iconBudgets },
-  POTS: { route: "/pots", icon: iconPots },
-};
+export const menuCategories = [
+  { name: "OVERVIEW", route: "/overview", icon: iconOverview },
+  { name: "TRANSACTIONS", route: "/transactions", icon: iconTransactions },
+  { name: "BUDGETS", route: "/budgets", icon: iconBudgets },
+  { name: "POTS", route: "/pots", icon: iconPots },
+];
