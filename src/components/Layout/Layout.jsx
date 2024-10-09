ccd78466ae86c4fff53f09680b7037b9bb53265b
Mateusz Kuchta <mtkuchta@gmail.com>
2024-10-09 19:59:14 +0200

    create resize window listener

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,37 @@
 import styles from "./Layout.module.css";
+
+import { useEffect, useState } from "react";
+import debounce from "lodash/debounce";
+
 import { MenuBar } from "../MenuBar/MenuBar";
 
 export function Layout() {
+  const [windowSize, setWindowSize] = useState("desktop");
+
+  useEffect(() => {
+    const handleResizeWindow = debounce(() => {
+      const currentWindowWidth = window.innerWidth;
+
+      if (currentWindowWidth < 768) {
+        setWindowSize("mobile");
+      } else if (currentWindowWidth < 1024) {
+        setWindowSize("tablet");
+      } else {
+        setWindowSize("desktop");
+      }
+    }, 50);
+
+    window.addEventListener("resize", handleResizeWindow);
+
+    return () => {
+      window.removeEventListener("resize", handleResizeWindow);
+    };
+  }, []);
+
   return (
     <div className={styles.wrapper}>
       <main></main>
-      <MenuBar />
+      <MenuBar windowSize={windowSize} />
     </div>
   );
 }
