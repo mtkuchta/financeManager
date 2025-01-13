802ef9b03395ce09fcc910cc370e14517c24d22a
Mateusz Kuchta <mtkuchta@gmail.com>
2025-01-13 19:37:35 +0100

    STYLES: upgrade Button styles, add posibility of different appearance

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,5 +1,16 @@
 import styles from "./Button.module.css";
 
-export function Button({ text, onClick }) {
-  return <button onClick={onClick}>{text}</button>;
+export function Button({ text, onClick, isRed = false, isWide = false }) {
+  return (
+    <button
+      className={styles.button}
+      onClick={onClick}
+      style={{
+        backgroundColor: isRed ? "var(--color-red)" : "var(--color-grey-900)",
+        width: isWide ? "100%" : "",
+      }}
+    >
+      {text}
+    </button>
+  );
 }
